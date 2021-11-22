214b13f9a26eb8d87afbfd60b1980edeab61ad10
Zenobia Gawlikowska <jgawlikowski@ecovadis.com>
2021-11-22 13:36:27 +0100

    a11y improvements

--- a11y/src/App.js
+++ a11y/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <>
-      <header>Accessibility demo page</header>
+      <header role="banner">
+        <h1>
+          Accessibility demo page
+        </h1>
+        <form role="search">
+          <input type="search" aria-label="search text" size="20"/>
+          <input type="submit" value="Search"/>
+        </form>
+      </header>
       <div className="container">
         <nav className="menu" role="navigation">
           <ul>
@@ -17,7 +24,7 @@ function App() {
             <li>Menu item 7</li>
           </ul>
         </nav>
-        <main className="main" role="main">
+        <main className="main" role="main" label="Main content">
           <p>This is the main content of the page</p>
 
           <div role="region" aria-labelledby="special-region">
@@ -25,7 +32,7 @@ function App() {
             This is a region that I want the user to be able to find.
           </div>
         </main>
-        <footer className="footer" role="complementary">
+        <footer className="footer" role="contentinfo">
           <p>This is a <a href="#">Footer link</a></p>
         </footer>
       </div>
