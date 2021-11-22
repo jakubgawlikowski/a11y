383e0186b76d7da7c2b5fa2651dd5e03c32a3180
Zenobia Gawlikowska <jgawlikowski@ecovadis.com>
2021-11-22 12:32:03 +0100

    adding landmarks

--- a11y/src/App.js
+++ a11y/src/App.js
@@ -3,23 +3,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <ul className="menu">
-        <li>Menu item 1</li>
-        <li>Menu item 2</li>
-        <li>Menu item 3</li>
-        <li>Menu item 4</li>
-        <li>Menu item 5</li>
-        <li>Menu item 6</li>
-        <li>Menu item 7</li>
-      </ul>
-      <div className="main">
-        <p>This is the main content of the page</p>
+    <>
+      <header>Accessibility demo page</header>
+      <div className="container">
+        <nav className="menu" role="navigation">
+          <ul>
+            <li>Menu item 1</li>
+            <li>Menu item 2</li>
+            <li>Menu item 3</li>
+            <li>Menu item 4</li>
+            <li>Menu item 5</li>
+            <li>Menu item 6</li>
+            <li>Menu item 7</li>
+          </ul>
+        </nav>
+        <main className="main" role="main">
+          <p>This is the main content of the page</p>
+
+          <div role="region" aria-labelledby="special-region">
+            <p id="special-region">Special region</p>
+            This is a region that I want the user to be able to find.
+          </div>
+        </main>
+        <footer className="footer" role="complementary">
+          <p>This is a <a href="#">Footer link</a></p>
+        </footer>
       </div>
-      <div className="footer">
-        <p>This is a <a href="#">Footer link</a></p>
-      </div>
-    </div>
+    </>
   );
 }
 
