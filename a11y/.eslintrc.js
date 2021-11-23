f9093ce80b63b1adca46dded4bdfb64145797f95
Zenobia Gawlikowska <jgawlikowski@ecovadis.com>
2021-11-23 11:54:15 +0100

    adding testing

--- a11y/.eslintrc.js
+++ a11y/.eslintrc.js
@@ -1,19 +1,22 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "jest": true
     },
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
         "plugin:jsx-a11y/strict"
     ],
+    "parser": "babel-eslint",
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
         },
         "ecmaVersion": 7,
-        "sourceType": "module"
+        "sourceType": "module",
+        "allowImportExportEverywhere": true
     },
     "plugins": [
         "react",
---
+++ a11y/src/App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { axe } from 'jest-axe';
+// eslint-disable-next-line no-unused-vars
+import { toHaveNoViolations } from 'jest-axe';
+
+import App from './App';
+
+expect.extend(toHaveNoViolations);
+
+describe('MenuButton', () => {
+  it('checks a11y rules', async () => {
+    const { container } = render(
+      <App />,
+    );
+
+    expect(await axe(container)).toHaveNoViolations();
+  });
+});
