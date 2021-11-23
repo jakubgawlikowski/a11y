03380aad88598e40eabd2f881790234085df51d9
Zenobia Gawlikowska <jgawlikowski@ecovadis.com>
2021-11-23 16:38:23 +0100

    changing element order

--- a11y/src/App.js
+++ a11y/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
 import React from 'react';
 import { StyledEngineProvider } from '@mui/material/styles';
 import Modal from './Modal';
@@ -10,27 +11,13 @@ function App() {
   return (
     <StyledEngineProvider injectFirst>
       <header role="banner">
-        <h1>
+        <h1 tabIndex="0">
           Accessibility demo page
         </h1>
       </header>
-      <form role="search">
-        <input type="search" aria-label="search text" size="20"/>
-        <input type="submit" value="Search"/>
-      </form>
+
       <div className="container">
-        <nav className="menu" role="navigation">
-          <ul>
-            <li><Modal/></li>
-            <li><Dialog /></li>
-            <li><Tooltip tabIndex="0" /></li>
-            <li>Menu item 4</li>
-            <li>Menu item 5</li>
-            <li>Menu item 6</li>
-            <li>Menu item 7</li>
-          </ul>
-        </nav>
-        <main className="main" role="main" aria-label="Main content">
+        <main className="main" role="main" aria-label="Labelled as Main content" id="main-content" tabIndex="0">
           <h3>This is the main content of the page</h3>
 
           <section aria-labelledby="special-region">
@@ -52,14 +39,29 @@ function App() {
             </p>
           </section>
         </main>
+        <nav className="menu" role="navigation">
+          <ul>
+            <li><Modal/></li>
+            <li><Dialog /></li>
+            <li><Tooltip tabIndex="0" /></li>
+            <li>Menu item 4</li>
+            <li>Menu item 5</li>
+            <li>Menu item 6</li>
+            <li>Menu item 7</li>
+          </ul>
+        </nav>
         <footer className="footer" role="contentinfo" aria-labelledby="footer-label">
           <p id="footer-label">Links in the footer</p>
-          <p><a href="/one">This is the first link.</a></p>
+          <p><a href="#main-content">Go to main content.</a></p>
           <p><a href="/two">This is the second link.</a></p>
           <p><a href="/three">This is the third link.</a></p>
           <p><a href="/four">This is the fourth link.</a></p>
         </footer>
       </div>
+      <form role="search" className="search">
+        <input type="search" aria-label="search text" size="20"/>
+        <input type="submit" value="Search"/>
+      </form>
     </StyledEngineProvider>
   );
 }
