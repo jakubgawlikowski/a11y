e9ac2db38adf60394fc6e3b3d1bd0abbf6e630c7
Zenobia Gawlikowska <jgawlikowski@ecovadis.com>
2021-11-23 10:41:03 +0100

    removed landmarks

--- a11y/src/App.js
+++ a11y/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <ul className="menu">
-        <li>Menu item 1</li>
-        <li>Menu item 2</li>
-        <li>Menu item 3</li>
-        <li>Menu item 4</li>
-        <li>Menu item 5</li>
-        <li>Menu item 6</li>
-        <li>Menu item 7</li>
-      </ul>
-      <div className="main">
-        <p>This is the main content of the page</p>
+    <>
+      <header role="banner">
+        <h1>
+          Accessibility demo page
+        </h1>
+      </header>
+      <form>
+        <input type="search" aria-label="search text" size="20"/>
+        <input type="submit" value="Search"/>
+      </form>
+      <div className="container">
+        <nav className="menu">
+          <ul>
+            <li>Menu item 1</li>
+            <li>Menu item 2</li>
+            <li>Menu item 3</li>
+            <li>Menu item 4</li>
+            <li>Menu item 5</li>
+            <li>Menu item 6</li>
+            <li>Menu item 7</li>
+          </ul>
+        </nav>
+        <main className="main">
+          <h2>This is the main content of the page</h2>
+
+          <section>
+            <h3 id="special-region">Special region</h3>
+            <p>
+            This is a region that I want the user to be able to find.
+            </p>
+          </section>
+          <section>
+            <h3 id="special-region2">Some other region</h3>
+            <p>
+            This is a another generic region.
+            </p>
+          </section>
+          <section>
+            <h3 id="special-region3">Yet another region</h3>
+            <p>
+            This is still another generic region.
+            </p>
+          </section>
+        </main>
+        <footer className="footer">
+          <p>This is the first <a href="/">link</a>.</p>
+          <p>This is the second <a href="/">link</a>.</p>
+          <p>This is the third <a href="/">link</a>.</p>
+          <p>This is the fourth <a href="/">link</a>.</p>
+        </footer>
       </div>
-      <div className="footer">
-        <p>This is a <a href="#">Footer link</a></p>
-      </div>
-    </div>
+    </>
   );
 }
 
