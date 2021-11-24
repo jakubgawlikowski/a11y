52385e1483bd905b19dc38c79a789e5ac745772a
Zenobia Gawlikowska <jgawlikowski@ecovadis.com>
2021-11-24 21:14:45 +0100

    css lint

--- a11y/src/App.test.js
+++ a11y/src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { axe } from 'jest-axe';
+// eslint-disable-next-line no-unused-vars
+import { toHaveNoViolations } from 'jest-axe';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+expect.extend(toHaveNoViolations);
+
+describe('MenuButton', () => {
+  it('checks a11y rules', async () => {
+    const { container } = render(
+      <App />,
+    );
+
+    expect(await axe(container)).toHaveNoViolations();
+  });
 });
