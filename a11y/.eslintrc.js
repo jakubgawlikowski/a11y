ea025015c1bfd2e1c086bc4b1160c9e9aa5df98a
Zenobia Gawlikowska <jgawlikowski@ecovadis.com>
2021-11-28 13:21:46 +0100

    adding deps

--- a11y/.eslintrc.js
+++ a11y/.eslintrc.js
@@ -1,21 +1,26 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "jest": true
     },
     "extends": [
         "eslint:recommended",
-        "plugin:react/recommended"
+        "plugin:react/recommended",
+        "plugin:jsx-a11y/strict"
     ],
+    "parser": "babel-eslint",
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
         },
-        "ecmaVersion": 8,
-        "sourceType": "module"
+        "ecmaVersion": 7,
+        "sourceType": "module",
+        "allowImportExportEverywhere": true
     },
     "plugins": [
-        "react"
+        "react",
+        "jsx-a11y"
     ],
     "rules": {
     }
